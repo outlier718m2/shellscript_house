bbe7a17e6c241b007a8c2878f5d85e4001215078
718m2 <wjdgydks@naver.com>
2025-02-26 18:02:23 +0900

    zip 파일 확장

--- backup_script_hj.sh
+++ backup_script_hj.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+# 패치파일 이름을 입력 받음
 read -p "패치파일을 입력하세요(jar확장자 제외): " patch_name
 
-FILE_LIST = $(jar tf "$patch_name.jar")
-OUTPUT_JAR = "${patch_name}_bak.jar"
+# 패치파일이 zip 파일인지 확인
+if file "$patch_name.zip" | grep -q "Zip archive data"; then
+    C_PATCH_NAME="$patch_name.zip"
+else
+    C_PATCH_NAME="$patch_name.jar"
+    exit 1
+fi
 
-jar cvf "$OUTPUT_JAR" -C "$(dirname "$patch_name.jar")" $(echo "$FILE_LIST")
+# jar 파일 내의 파일 목록을 가져오고 디렉토리만 제외
+FILE_LIST=$(jar tf "$C_PATCH_NAME" | grep -v '/$')
 
+# 출력할 새로운 JAR 파일 이름 설정
+OUTPUT_JAR="${patch_name}_bak.jar"
+
+# JAR 파일을 새로 생성
+jar cvf "$OUTPUT_JAR" -C "$(dirname "$C_PATCH_NAME")" $FILE_LIST
+
+# 완료 메시지 출력
 echo "JAR 파일(백업)이 생성되었습니다: $OUTPUT_JAR"
